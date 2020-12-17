c6aca8c314296cc2021ba21a7a2a699057ee628f
avidev <avirevah32@gmail.com>
2020-12-17 10:14:26 +0200

    responsive menu work

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import '../styles/header.css'
 import { Link } from "react-router-dom";
+import CloseIcon from '@material-ui/icons/Close';
+import InstagramIcon from '@material-ui/icons/Instagram';
 
 const Header = () => {
-
+  const [ showMobileMenu, setShowMobileMenu ] = useState(false)
   const [ show, setShow ] = useState(false)
 
   useEffect(() => {
@@ -33,12 +35,27 @@ const Header = () => {
                 <li><Link to="/">Beauty</Link></li>
               </ul>
             </div>
-            <div className="burger">
+            <div className="burger" onClick={() => setShowMobileMenu(true)}>
               <span></span>
               <span></span>
               <span></span>
             </div>
-            
+            {showMobileMenu && 
+              <nav className="mobile_menu">
+                <CloseIcon id="close_menu_icon" onClick={() => setShowMobileMenu(false)}/>
+                <ul>
+                  <li><Link to="/">Beauty</Link></li>
+                  <li><Link to="/">Body & Soul</Link></li>
+                  <li><Link to="/">Food</Link></li>
+                  <li><Link to="/">Life</Link></li>
+                  <li><Link to="/">Contact</Link></li>
+                </ul>
+                <div className="social">
+                  <InstagramIcon />
+                </div>
+              </nav>
+
+            }
         </div>
     </div>
   )
